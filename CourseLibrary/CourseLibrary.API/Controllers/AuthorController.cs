24cb0cc49bc6c04019128fd877b51cf2d271a681
MertOzerdem <32201738+MertOzerdem@users.noreply.github.com>
2020-02-21 18:12:23 +0300

    version1

---
+++ CourseLibrary/CourseLibrary.API/Controllers/AuthorController.cs
@@ -0,0 +1,49 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController] // returns auto respons contexts like 404 not found
+    [Route("api/authors")]
+    public class AuthorController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository courseLibraryRepository;
+
+        public AuthorController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            this.courseLibraryRepository = courseLibraryRepository ?? 
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet("")]
+        public IActionResult GetAuthors() // IAction returns action result
+        {
+            var authorsFromRepo = this.courseLibraryRepository.GetAuthors();
+            return Ok(authorsFromRepo);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            // Alternative method to check if author exist
+            //if (!this.courseLibraryRepository.AuthorExists(authorId))
+            //{
+            //    return NotFound();
+            //}
+            // A better  way to do is
+            var authorFromRepo = this.courseLibraryRepository.GetAuthor(authorId);
+
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(authorFromRepo);
+            
+        }
+    }
+}
